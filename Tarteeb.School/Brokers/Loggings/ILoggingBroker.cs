ad3e3cfe50539f55f7714535934bd58cd23f6923
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-21 10:13:59 +0500

    BROKERS: Add Logging

---
+++ Tarteeb.School/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Tarteeb.School.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
